3cd152c833c6edbb8f410987aad3d60a41fa3eef
Luis Tolentino <luistolentino27@gmail.com>
2020-07-02 15:32:27 -0400

    Add project files.

---
+++ GetDataAjax/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using GetDataAjax.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls.WebParts;
+
+namespace GetDataAjax.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public JsonResult GetPerson(int? id)
+        {
+            //Simulando data
+            var personList = new List<Persona>();
+            personList.Add(new Persona { Id = 1, Name = "Alonso", Age = 23, IsAlive = true });
+            personList.Add(new Persona { Id = 2, Name = "Gabriela", Age = 32, IsAlive = false });
+            personList.Add(new Persona { Id = 3, Name = "Wen", Age = 25, IsAlive = true });
+            personList.Add(new Persona { Id = 4, Name = "Julio", Age = 50, IsAlive = false });
+
+            Persona result = null;
+            if(id.HasValue)
+            {
+                result = personList.Where(p => p.Id == id.Value).FirstOrDefault();
+            }
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ GetDataAjax/Models/Persona.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GetDataAjax.Models
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public bool IsAlive { get; set; }
+    }
+}
